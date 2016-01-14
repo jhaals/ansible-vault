9f02929673389884d4dd261964b7b1be6c959caa
Johan Haals <johan.haals@gmail.com>
2016-01-14 17:37:55 +0100

    Update plugin for ansible 2.0

--- vault.py
+++ vault.py
@@ -4,37 +4,33 @@ import json
 import sys
 from urlparse import urljoin
 
-from ansible import utils, errors
-from ansible.utils import template
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
 
-class LookupModule(object):
 
-    def __init__(self, basedir=None, **kwargs):
-        self.basedir = basedir
+class LookupModule(LookupBase):
 
-    def run(self, terms, inject=None, **kwargs):
-
-        try:
-            terms = template.template(self.basedir, terms, inject)
-        except Exception, e:
-            pass
+    def run(self, terms, variables, **kwargs):
+        key = terms[0]
 
         url = os.getenv('VAULT_ADDR')
         if not url:
-            raise errors.AnsibleError('VAULT_ADDR environment variable is missing')
+            raise AnsibleError('VAULT_ADDR environment variable is missing')
 
         token = os.getenv('VAULT_TOKEN')
         if not token:
-            raise errors.AnsibleError('VAULT_TOKEN environment variable is missing')
+            raise AnsibleError('VAULT_TOKEN environment variable is missing')
 
-        request_url = urljoin(url, "v1/%s" % (terms))
+        request_url = urljoin(url, "v1/%s" % (key))
         try:
             headers = { 'X-Vault-Token' : token }
             req = urllib2.Request(request_url, None, headers)
             response = urllib2.urlopen(req)
         except urllib2.HTTPError as e:
-            raise errors.AnsibleError('Unable to read %s from vault: %s' % (terms, e))
+            raise AnsibleError('Unable to read %s from vault: %s' % (key, e))
         except:
-            raise errors.AnsibleError('Unable to read %s from vault' % terms)
+            raise AnsibleError('Unable to read %s from vault' % key)
+
         result = json.loads(response.read())
+
         return [result['data']['value']]
